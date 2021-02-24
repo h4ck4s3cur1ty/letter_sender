267f4d19448085a3f1077aed609baa6858da42d2
h4ck4s3cur1ty <qjawns0117@gmail.com>
2021-02-25 02:16:27 +0900

    Update letter_sender_katc.py
    Add naver_baseball_sk
    Add epl_rank
    Modify name

--- letter_sender_katc.py
+++ letter_sender_katc.py
@@ -31,7 +31,7 @@ def wait_auth():
         else:
             print('no')
 
-def write_letter(title, content, password):
+def send_letter(title, content, password):
     write_title = driver.find_element_by_id("article_title")
     write_title.send_keys(title)
     write_content = driver.find_element_by_id("article_text")
@@ -61,7 +61,7 @@ def send_boannews():
         pages = math.ceil(len(body) / 800)
         body = [body[i:i+800] for i in range(0,len(body), 800)]
         for j in range(pages):
-            write_letter(title + ' - ' + str(j), body[j].strip(), letter_password)
+            send_letter(title + ' - ' + str(j+1), body[j].strip(), letter_password)
             time.sleep(0.5)
             turnoff_alert()
             time.sleep(0.5)
@@ -83,11 +83,54 @@ def send_jtbcnews():
         pages = math.ceil(len(body) / 800)
         body = [body[i:i+800] for i in range(0,len(body), 800)]
         for j in range(pages):
-            write_letter(title + ' - ' + str(j), body[j].strip(), letter_password)
+            send_letter(title + ' - ' + str(j+1), body[j].strip(), letter_password)
             time.sleep(0.5)
             turnoff_alert()
             time.sleep(0.5)
             init(enlistment_date,birth,name)
+
+def get_naver_baseball_sk_content(oid, aid):
+    r = requests.get('https://sports.news.naver.com/news.nhn?oid=' + oid +'&aid=' + aid)
+    soup = BeautifulSoup(r.content, 'lxml')
+    data = str(soup.find('div', id='newsEndContents'))
+    body = data[:data.find('<p class="source">')]
+    body = re.sub('<.+?>', '', body).strip()
+    return body
+
+def send_naver_baseball_sk():
+    r = requests.get('https://sports.news.naver.com/kbaseball/news/list.nhn?isphoto=N&type=team&team=SK&view=text')
+    data = r.json()
+    for i in range(len(data['list'])):
+        title = data['list'][i]['title'].strip()
+        oid = data['list'][i]['oid']
+        aid = data['list'][i]['aid']
+        body = get_naver_baseball_sk_content(oid, aid)
+        pages = math.ceil(len(body) / 800)
+        body = [body[i:i+800] for i in range(0,len(body), 800)]
+        for j in range(pages):
+            send_letter(title + ' - ' + str(j+1), body[j].strip(), letter_password)
+            time.sleep(0.5)
+            turnoff_alert()
+            time.sleep(0.5)
+            init(enlistment_date,birth,name)
+
+def send_epl_rank():
+    r = requests.get('https://sports.news.naver.com/wfootball/index.nhn')
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    now = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+    team_list = soup.find('div', id='_team_rank_epl').findAll('tr')
+    result = ''
+    for i in range(1,len(team_list)):
+        rank = team_list[i].find('span', class_='blind').contents[0]
+        team = team_list[i].find('span', class_='name').contents[0]
+        score = team_list[i].findAll('span')[7].contents[0]
+        result += "{} {} {}".format(rank,team,score)+' ## '
+    send_letter(now + ' EPL 순위', result, letter_password)
+    time.sleep(0.5)
+    turnoff_alert()
+    time.sleep(0.5)
+    init(enlistment_date,birth,name)
 
 URL = 'https://www.katc.mil.kr/katc/community/children.jsp'
 
@@ -100,5 +143,8 @@ driver = webdriver.Chrome(executable_path='chromedriver')
 driver.get(url=URL)
 init(enlistment_date,birth,name)
 wait_auth()
+
 send_jtbcnews()
 send_boannews()
+send_naver_baseball_sk()
+send_epl_rank()
